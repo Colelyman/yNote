75742f7867388582661b0820eebc26d906a2b9e2
Eric Seamons <ericseamons90@gmail.com>
2015-11-11 22:59:32 +0000

    index.html now uses angular to get body of note from controller

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,11 +4,8 @@ angular.module('yNoteApp', [])
   function($scope, $http){
 
     $scope.notes = [
-      {title:'Test Comment', content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar rhoncus risus, vel ornare lacus sagittis sit amet. Duis vel sem magna. Proin pulvinar velit eleifend ligula ultrices vestibulum. Nunc posuere dolor eu mauris feugiat dignissim.'}
+      {title:'Test Comment', body:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris pulvinar rhoncus risus, vel ornare lacus sagittis sit amet. Duis vel sem magna. Proin pulvinar velit eleifend ligula ultrices vestibulum. Nunc posuere dolor eu mauris feugiat dignissim.'}
     ];
-
-    $scope.test = 'Hello world!';
-
 
 
 
