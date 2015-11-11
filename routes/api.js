2e9ade7bdb22c7a1dcbf795114b8ce4900b11333
Cole Lyman <cole@colelyman.com>
2015-11-11 14:30:13 -0700

    Added routes to GET notes and POST notes. Convereted DOS newlines to UNIX

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,28 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Note = mongoose.model('Note');
 
-/* GET note json */
+/* GET json list of all notes */
 router.get('/notes', function(req, res, next) {
   Note.find(function(err, notes) {
     if(err) { return next(err); }
     res.json(notes);
   });
+});
+
+/* GET json of one note */
+
+router.param('note', function(req, res, next, id) {
+  var query = Note.findById(id);
+  query.exec(function(err, note) {
+    if(err) { return next(err); }
+    if(!note) { return next(new Error("The note cannot be found!!")); }
+    req.note = note;
+    return next();
+  });
+});
+
+router.get('/notes/:note', function(req, res) {
+  res.json(req.note);
 });
 
 /* POST note json */
